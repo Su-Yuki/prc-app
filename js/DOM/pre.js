0a5b4cbffe2e9b33bd0566fc3bf0b48419f2225f
Su-Yuki <76637981+Su-Yuki@users.noreply.github.com>
2021-01-31 21:56:13 +0900

    firast comit

---
+++ js/DOM/pre.js
@@ -0,0 +1,21 @@
+// IDを指定して要素を取得
+// let blueEl = document.getElementById('blue');
+// console.log(blueEl);
+
+// CLASSを指定して要素を取得
+// document.getElementsByClassName('class名')
+// let greenEl = document.getElementsByClassName('green');
+// console.log(greenEl);
+
+// タグ名を指定して要素を取得
+// document.getElementsByTagName('タグ名');
+
+// let liEl = document.getElementsByTagName('li');
+// console.log(liEl);
+
+let red = document.getElementById('red');
+let ul = red.parentElement;
+console.log(ul);
+
+let box = document.getElementById('box');
+console.log(box);
