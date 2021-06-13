3e529329524901b0bfa62de04fea4a7eae2c8fc9
Su-Yuki <76637981+Su-Yuki@users.noreply.github.com>
2021-06-13 23:48:57 +0900

    no message

---
+++ parts/check/branch.php
@@ -0,0 +1,7 @@
+<?php 
+if(!$_POST["text"]){
+    echo "ボックスが空です";
+    '<a href="./index.php"><button>戻る</button></a>';
+    // header("location: index.php");
+}
+?>
---
+++ parts/check/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="./branch.php">
+    <input type="text" naem="text">
+    <button type="submit">確認する</button>
+</form>    
+</body>
+</html>
